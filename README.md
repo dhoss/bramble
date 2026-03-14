4f5f92aa5b65809ec553a39c55db452bb1c36070
Devin Austin <devin.austin@gmail.com>
2026-03-14 15:33:18 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bramble
+
+A threaded forum
---
+++ src/main/java/in/stonecoldev/frontend/HomeController.java
@@ -0,0 +1,18 @@
+package in.stonecoldev.frontend;
+
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+
+@Controller("/")
+public class HomeController {
+
+  @Get
+  @Produces(MediaType.TEXT_PLAIN)
+  public String index() {
+    return "frrrrrrrrrt";
+  }
+
+}
